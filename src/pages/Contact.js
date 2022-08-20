c23b97de49553c556df0ef44c4987d81c45330bb
r-alo <email@example.com>
2022-08-19 23:55:42 -0500

    created contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Container from 'react-bootstrap/esm/Container';
+import FloatingLabel from 'react-bootstrap/esm/FloatingLabel';
+
+function Contact () {
+    return (
+        <Container>
+        <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+                
+                <Form.Control type="text" placeholder="Name" validated />
+                <br/>
+                <Form.Control type="email" placeholder="Email" />
+                <br/>
+                <Form.Control type="number" placeholder="Phone Number" />
+                <br />
+                <Form.Control as="textarea" placeholder="Thoughts?"
+                            style={ { height: '100px' } }
+                        />
+            </Form.Group>
+            <Button variant="outline-primary" type="submit" className='button'>
+                Submit
+            </Button>
+        </Form>
+        </Container>
+    );
+}
+
+export default Contact;
